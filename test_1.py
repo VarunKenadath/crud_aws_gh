f9bb9eb069412f8d139bff10bd30891c84bcb28d
VarunKenadath <varun.kenadath@gmail.com>
2026-05-13 18:01:45 +1000

    fix errors

--- test_1.py
+++ test_1.py
@@ -12,7 +12,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
-engine=create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, 
+    connect_args={"check_same_thread": False}
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base=declarative_base()
 # sessionmaker:class factory, returns a class when instantiated creates a new Session
