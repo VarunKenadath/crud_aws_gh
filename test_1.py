61df1f84064641dbce7716f554f00bebbaf821dc
VarunKenadath <varun.kenadath@gmail.com>
2026-05-13 18:07:29 +1000

    fix errors again

--- test_1.py
+++ test_1.py
@@ -11,7 +11,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = "sqlite:////tmp/todos.db"
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, 
     connect_args={"check_same_thread": False}
